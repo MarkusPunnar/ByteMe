72fb38eed988536c00ac3c7b2de1bdd4eccbe420
Markus Punnar <markus.punnar@gmail.com>
2018-09-03 22:53:53 +0300

    Added basic project structure

--- src/main/java/byteMe/ByteMeApplication.java
+++ src/main/java/byteMe/ByteMeApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
 @SpringBootApplication
 public class ByteMeApplication extends SpringBootServletInitializer {
 
-  public static void main(String[] args) {
-    SpringApplication.run(ByteMeApplication.class, args);
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(ByteMeApplication.class, args);
+    }
 }
