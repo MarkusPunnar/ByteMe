d33781ae44b0a8ba67158d5837e24c0cfa68acdf
Markus Punnar <markus.punnar@gmail.com>
2018-09-04 12:54:28 +0300

    More structure and first page shell

--- src/main/java/byteMe/ByteMeApplication.java
+++ src/main/java/byteMe/ByteMeApplication.java
@@ -2,10 +2,9 @@ package byteMe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ByteMeApplication extends SpringBootServletInitializer {
+public class ByteMeApplication  {
 
     public static void main(String[] args) {
         SpringApplication.run(ByteMeApplication.class, args);
