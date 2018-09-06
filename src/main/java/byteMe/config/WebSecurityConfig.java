5ce2a40f86669b74836e11e011777010631a3635
Markus Punnar <markus.punnar@gmail.com>
2018-09-06 13:45:24 +0300

    Added boilerplate html files

---
+++ src/main/java/byteMe/config/WebSecurityConfig.java
@@ -0,0 +1,20 @@
+package byteMe.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/register", "/about", "/tutorial", "/", "/css/*", "/images/*").permitAll()
+                .anyRequest().authenticated().and()
+                .formLogin().loginPage("/login").permitAll().and().logout().permitAll();
+    }
+}
