0db0a2565edbbd75226caa836b760768dc61e346
Markus Punnar <markus.punnar@gmail.com>
2018-09-08 12:01:00 +0300

    Updates to login

--- src/main/java/byteMe/config/WebSecurityConfig.java
+++ src/main/java/byteMe/config/WebSecurityConfig.java
@@ -2,9 +2,12 @@ package byteMe.config;
 
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -16,5 +19,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/register", "/about", "/tutorial", "/", "/css/*", "/images/*").permitAll()
                 .anyRequest().authenticated().and()
                 .formLogin().loginPage("/login").permitAll().and().logout().permitAll();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        auth.inMemoryAuthentication().withUser("user")
+                .password(encoder.encode("password")).authorities("USER");
     }
 }
