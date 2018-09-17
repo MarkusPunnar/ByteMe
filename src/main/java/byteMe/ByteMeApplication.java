fe8a67703dd4f36e1a5c07fb6d4253b3fbb40863
Markus Punnar <markus.punnar@gmail.com>
2018-09-17 21:22:32 +0300

    Removed hard-coded passwords from source code

--- src/main/java/byteMe/ByteMeApplication.java
+++ src/main/java/byteMe/ByteMeApplication.java
@@ -3,6 +3,7 @@ package byteMe;
 import byteMe.services.RoomRepository;
 import org.jdbi.v3.core.Jdbi;
 import org.jdbi.v3.sqlobject.SqlObjectPlugin;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -12,13 +13,18 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @SpringBootApplication
 public class ByteMeApplication {
 
+    @Value("${spring.datasource.username}")
+    private String dbUser;
+    @Value("${spring.datasource.password}")
+    private String dbPassword;
+
     public static void main(String[] args) {
         SpringApplication.run(ByteMeApplication.class, args);
     }
 
     @Bean
     Jdbi jdbi() {
-        Jdbi jdbi = Jdbi.create("jdbc:mysql://localhost:3306/byteme", "byteme", "lo102llu33");
+        Jdbi jdbi = Jdbi.create("jdbc:mysql://localhost:3306/byteme", dbUser, dbPassword);
         jdbi.installPlugin(new SqlObjectPlugin());
         return jdbi;
     }
