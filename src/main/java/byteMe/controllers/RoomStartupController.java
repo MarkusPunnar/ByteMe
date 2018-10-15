532912ccb8b196d1613e149bb243c2f9717dead7
Markus Punnar <markus.punnar@gmail.com>
2018-10-15 14:52:08 +0300

    File uploading

--- src/main/java/byteMe/controllers/RoomStartupController.java
+++ src/main/java/byteMe/controllers/RoomStartupController.java
@@ -9,16 +9,22 @@ import byteMe.services.GameInstanceService;
 import byteMe.services.RoomStartupRepository;
 import org.jdbi.v3.core.Jdbi;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 @Controller
 @RequestMapping("/session")
 public class RoomStartupController {
 
+    @Value("${instanceFileLocation}")
+    private String instanceFilePath;
     private final GameInstanceService instanceService;
     private final Jdbi jdbi;
     private final AuthService authService;
@@ -31,7 +37,8 @@ public class RoomStartupController {
     }
 
     @RequestMapping(value = "/create", method = RequestMethod.POST)
-    public String createRoom(@RequestParam("assessment") List<String> instanceElements, Model model) {
+    public String createRoom(@RequestParam("assessment") List<String> instanceElements,
+            @RequestParam("picture") List<MultipartFile> instancePictures, Model model) throws IOException {
         authService.addAuthInfoToModel(model);
         return jdbi.inTransaction(handle -> {
             RoomStartupRepository repository = handle.attach(RoomStartupRepository.class);
@@ -43,7 +50,12 @@ public class RoomStartupController {
             int hostID = repository.getUserID(displayname);
             repository.addRoom(roomID, hostID, instanceElements.size());
             for (String instanceElement : instanceElements) {
-                repository.addElement(roomID, instanceElement);
+                repository.addElement(roomID, instanceElement, "text");
+            }
+            for (int i = 0; i < instancePictures.size(); i++) {
+                String picturePath = instanceFilePath + roomID  + "_" + (i+1) + ".png";
+                instancePictures.get(i).transferTo(new File(picturePath));
+                repository.addElement(roomID, picturePath, "picture");
             }
             String hostname = repository.getHostName(roomID);
             model.addAttribute("roomID", roomID);
